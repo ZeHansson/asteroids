952ea277e34e3efd27fc7e167b5944b24b433192
ZeHansson <chriszehansson@gmail.com>
2025-01-24 22:24:44 +0100

    5: added non lethal asteroids!

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -15,10 +17,27 @@ def main():
     
     fps_limiter = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    
+
+    
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
+ 
+    
 
 
     while True:
@@ -30,12 +49,16 @@ def main():
         #1
 
 
-        #2
+        #2 Update!
+        for entity in updatable:
+            entity.update(dt)
 
         #3 Draw!
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
+        
+        
         
         pygame.display.flip()
         dt = fps_limiter.tick(60)/1000
--- player.py
+++ player.py
@@ -5,6 +5,7 @@ from constants import *
 
 
 class Player(CircleShape):
+    
     def __init__(self, x, y ):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
@@ -28,10 +29,20 @@ class Player(CircleShape):
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
-            # ?
+            
         if keys[pygame.K_d]:
             self.rotate(dt)
-            # ?
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+            
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
 
 def main():
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
